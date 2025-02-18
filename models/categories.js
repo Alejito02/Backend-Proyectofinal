d38a6b644d989aa89f34984974ba66cf1bad2ad3
Andres sierra garcia <andresfelipe3666@gmail.com>
2025-02-18 10:07:17 -0500

    pequeños arreglos

--- models/categories.js
+++ models/categories.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
 const categoriesSchema = new mongoose.Schema({
-    name:{type:String, require:true},
-    description:{type:String, require:true},
-    createdAt:{type:Date, require:true, default:1},
+    name:{type:String, required:true},
+    description:{type:String, required:true},
+    createdAt:{type:Date, required:true, default:1},
 },{
     timestamps:true
 });
